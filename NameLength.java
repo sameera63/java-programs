21e9fc2400204699461ed98109a1078b20f63b5b
sameera63 <dudekulasameera05@gmail.com>
2025-08-19 16:11:39 +0530

    Add files via upload

---
+++ NameLength.java
@@ -0,0 +1,7 @@
+public class NameLength {
+	    public static void main(String[] args) {
+	        String name = "sameera";  
+	        int length = name.length();
+	        System.out.println("The length of the name \"" + name + "\" is: " + length);
+	    }
+	}
