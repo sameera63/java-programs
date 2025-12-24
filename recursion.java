d438f6c5e79d7a825bb27f037fac50ad08379b25
sameera63 <dudekulasameera05@gmail.com>
2025-12-24 14:11:30 +0530

    Add files via upload

--- recursion.java
+++ recursion.java
@@ -1,14 +1,15 @@
-public class recursion{
-	    public static long factorial(int n) {
-	        if (n == 0 || n == 1)
-	            return 1;
-	        else
-	            return n * factorial(n - 1);
-	    }
-
-	    public static void main(String[] args) {
-	        int num = 5;
-	        System.out.println("Factorial of " + num + " is: " + factorial(num));
-	    }
-	}
-
+class recursion
+{
+static int Fact(int n)
+{
+if(n==0)
+{
+return 1;
+}
+return n*Fact(n-1);
+}
+public static void main(String[] args)
+{
+	System.out.println(Fact(5));
+}
+}
